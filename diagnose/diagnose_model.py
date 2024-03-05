b6ef8261d1d95901707ab2340e441430a7b88255
Mohamed Abubaker <67129352+mohamedlecter@users.noreply.github.com>
2024-03-05 20:38:37 +0800

    comment plants model

--- diagnose/diagnose_model.py
+++ diagnose/diagnose_model.py
@@ -1,24 +1,24 @@
-import tensorflow as tf
-import json
-import os
+# import tensorflow as tf
+# import json
+# import os
 
-class PlantDiseaseModel:
-    def __init__(self, model_path='./plant_safe.h5', class_names_path='./class_names.json'):
-        # Get the absolute paths of the model and class names files
-        model_path = os.path.abspath(model_path)
-        class_names_path = os.path.abspath(class_names_path)
+# class PlantDiseaseModel:
+#     def __init__(self, model_path='./plant_disease_detector.h5', class_names_path='../class_names.json'):
+#         # Get the absolute paths of the model and class names files
+#         model_path = os.path.abspath(model_path)
+#         class_names_path = os.path.abspath(class_names_path)
 
-        # Load the trained model
-        self.model = tf.keras.models.load_model(model_path)
+#         # Load the trained model
+#         self.model = tf.keras.models.load_model(model_path)
 
-        # Load class names from JSON file
-        with open(class_names_path, 'r') as f:
-            self.class_names = json.load(f)
+#         # Load class names from JSON file
+#         with open(class_names_path, 'r') as f:
+#             self.class_names = json.load(f)
             
-    def predict(self, input_image):
-        # Implement prediction logic using the built-in predict function
-        prediction = self.model.predict(input_image)
-        return prediction
+#     def predict(self, input_image):
+#         # Implement prediction logic using the built-in predict function
+#         prediction = self.model.predict(input_image)
+#         return prediction
     
-    def get_class_name(self, class_index):
-        return self.class_names[class_index]
+#     def get_class_name(self, class_index):
+#         return self.class_names[class_index]
